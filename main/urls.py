bea2caf28882a9a18ba985228be43177e8a893a3
dPwls52 <limyj05201@gmail.com>
2026-06-18 02:55:09 +0900

    팟만들기 백엔드

--- main/urls.py
+++ main/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
 
     path('join_pot/', join_pot, name='join_pot'),
     path('new_pot/', new_pot, name='new_pot'), 
-    path('create_pot/', create_pot, name='create_pot'),
+    path('create/', create, name='create'),
 ]
